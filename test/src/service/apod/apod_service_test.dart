d0f03158df7cf12169fecf01fd3d7ecbb419bf05
myConsciousness <contact@shinyakato.dev>
2023-02-14 13:00:46 +0900

    test: fixed test case

--- test/src/service/apod/apod_service_test.dart
+++ test/src/service/apod/apod_service_test.dart
@@ -32,8 +32,21 @@ void main() {
 
       expect(response.data.copyright, 'Felipe Mac Auliffe L\u00f3pez');
       expect(response.data.createdAt, DateTime(2023, 2, 11));
-      expect(response.data.description,
-          'The two prominent clouds in this Chilean Atacama Desert skyscape captured on January 21 actually lie beyond our Milky Way galaxy.  Known as the Large and the Small Magellanic Clouds they are so named for the 16th century Portuguese explorer Ferdinand Magellan, leader of the first circumnavigation of planet Earth. Famous jewels of southern hemisphere skies, they are the brightest satellite galaxies of the Milky Way. The larger cloud is some 160,000 light-years, and the smaller 210,000 light-years distant. While both are irregular dwarf galaxies in their own right, they exhibit central barred structures in the deep wide-angle view. Wide and deep exposures also reveal faint dusty galactic cirrus nebulae and the imprints of gravitational tidal interactions between the Large and Small Magellanic Clouds.');
+      expect(
+          response.data.description,
+          'The two prominent clouds in this Chilean Atacama Desert skyscape '
+          'captured on January 21 actually lie beyond our Milky Way galaxy.  '
+          'Known as the Large and the Small Magellanic Clouds they are so '
+          'named for the 16th century Portuguese explorer Ferdinand Magellan, '
+          'leader of the first circumnavigation of planet Earth. Famous '
+          'jewels of southern hemisphere skies, they are the brightest '
+          'satellite galaxies of the Milky Way. The larger cloud is some '
+          '160,000 light-years, and the smaller 210,000 light-years distant. '
+          'While both are irregular dwarf galaxies in their own right, they '
+          'exhibit central barred structures in the deep wide-angle view. '
+          'Wide and deep exposures also reveal faint dusty galactic cirrus '
+          'nebulae and the imprints of gravitational tidal interactions '
+          'between the Large and Small Magellanic Clouds.');
       expect(response.data.hdUrl,
           'https://apod.nasa.gov/apod/image/2302/magellanic.jpg');
       expect(response.data.url,
